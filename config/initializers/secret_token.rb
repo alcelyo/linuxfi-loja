e0a345a11083161ed0dc5ddae026a32d11196c36
Mauricio Linhares <mauricio.linhares@gmail.com>
2011-10-18 22:15:00 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loja::Application.config.secret_token = '0aa5edfaa0b9f7c963e374486004ee0cf9bf4a966cba52365d8c3bb72efee17a36d3bb988c3e76462061bd52219984b39b9357e80c4b74ef049161542c4833c8'
