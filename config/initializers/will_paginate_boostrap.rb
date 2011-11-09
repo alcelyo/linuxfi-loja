f471177c060ec65ee872ea9d49236e4b5adf9b85
Mauricio Linhares <mauricio.linhares@gmail.com>
2011-11-08 21:40:42 -0300

    will paginate outra vez

--- config/initializers/will_paginate_boostrap.rb
+++ config/initializers/will_paginate_boostrap.rb
@@ -1,24 +1,26 @@
-module WillPaginate
-  module ActionView
-    def will_paginate(collection = nil, options = {})
-      options[:renderer] ||= BootstrapLinkRenderer
-      super.try :html_safe
-    end
+require 'will_paginate/view_helpers/link_renderer'
 
-    class BootstrapLinkRenderer < LinkRenderer
-      protected
+WillPaginate::ViewHelpers.class_eval do
 
-      def html_container(html)
-        tag :div, tag(:ul, html), container_attributes
-      end
+  def self.will_paginate(collection = nil, options = {})
+    options[:renderer] ||= BootstrapLinkRenderer
+    super.try :html_safe
+  end
 
-      def page_number(page)
-        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
-      end
+end
 
-      def previous_or_next_page(page, text, classname)
-        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
-      end
-    end
+class BootstrapLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
+  protected
+
+  def html_container(html)
+    tag :div, tag(:ul, html), container_attributes
+  end
+
+  def page_number(page)
+    tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+  end
+
+  def previous_or_next_page(page, text, classname)
+    tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
   end
 end
